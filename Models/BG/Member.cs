2bf418f470990af50a51759d51b821629b862d38
Transbug <tliao9@my.bcit.ca>
2021-10-15 02:54:18 -0700

    m1 commit

--- Models/BG/Member.cs
+++ Models/BG/Member.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+using Microsoft.AspNetCore.Authorization;
 #nullable disable
 
 namespace Lab03.Models.BG
@@ -18,6 +18,7 @@ namespace Lab03.Models.BG
         public string FirstName { get; set; }
         [Required]
         [MaxLength (15)]
+         [Display (Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
         [MaxLength (50)]
@@ -30,12 +31,16 @@ namespace Lab03.Models.BG
         public string Province { get; set; }
         [Required]
         [StringLength (6)]
+         [Display (Name = "Postal code")]
+
         public string PostalCode { get; set; }
         [Required]
         [MaxLength (10)]
+         [Display (Name = "Mobile Phone")]
         public string MobilePhone { get; set; }
         [Required]
         public string Email { get; set; }
+         [Display (Name = "Is Receive Email")]
         public bool IsReceiveEmail { get; set; }
     }
 }
